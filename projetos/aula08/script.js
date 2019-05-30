5452932e0befaab1530e801a39051c41da41c1a2
DJiNN-gh <allaneumermo@gmail.com>
2019-05-30 19:12:06 -0300

    Update Thursday 30 May 19:12 MacB

---
+++ projetos/aula08/script.js
@@ -0,0 +1,8 @@
+/*function alerta(){
+    
+    window.alert("Interrompe Carregamento!");
+    console.log("Verifique se apareceu");}
+
+function calculador_tempo(tLivre, tEstudo){
+    
+    return tLivre_i*100/(tEstudo_i+tLivre_i);} */
